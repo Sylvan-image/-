8c7ce1026101f6dcb307fe2c3e9c24a1b9db9ebe
Sylvan-image <sylvandocter@gmail.com@example.com>
2026-08-09 16:15:29 +0800

    最初版

---
+++ Assets/TutorialInfo/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private PlayerController playerControllerScript;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos=new Vector3(25,0,0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
